a4422858f745819a9dda8258c45cf14a09010714
Lucas Satabin <lucas.satabin@gnieh.org>
2013-02-07 18:06:23 +0100

    start modules when starting toris

--- src/toris.go
+++ src/toris.go
@@ -15,8 +15,20 @@ func main() {
 
   serverPort, _ := c.GetInt("server", "port")
 
+  log.Println("Modules", toris.Context.ModuleList())
   // Add default result formatter (JSON)
   goweb.ConfigureDefaultFormatters()
+
+  // start the modules
+  for name, _ := range(toris.Context.ModuleList()) {
+    log.Println("Starting module " + name)
+    err := toris.Context.Start(name)
+    if err != nil {
+      log.Println("[ERROR]", "An exception was raised", err)
+    } else {
+      log.Println("OK")
+    }
+  }
 
   serverUrl := fmt.Sprintf(":%d", serverPort)
   log.Println("Starting server on: " + serverUrl)
